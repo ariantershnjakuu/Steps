677d0a80517789feb0c6519265d804f08b8d7b94
ariantershnjakuu <at60058@ubt-uni.net>
2023-08-09 15:18:44 +0200

    Make next and previos buttons work

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,14 +1,47 @@
-import './App.css';
+import { useState } from "react";
+
+const message = [
+  "Learn React",
+  "Apply for jobs",
+  "Invest yor new income"
+]
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+  const [open, setOpen] = useState(true);
+
+  const handleNext = () => {
+    if (currentStep < 3) {
+      setCurrentStep(currentStep + 1)
+    }
+  }
+
+  const handlePrevious = () => {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          This is a React app.
-        </p>
-      </header>
-    </div>
+    <>
+      <button className="close" onClick={() => setOpen(!open)}>
+        &times;
+      </button>
+
+      {open && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={currentStep >= 1 ? "active" : ""}>1</div>
+            <div className={currentStep >= 2 ? "active" : ""}>2</div>
+            <div className={currentStep >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">Step {currentStep}: {message[currentStep - 1]}</p>
+          <div className="buttons">
+            <button onClick={handlePrevious}>Previos</button>
+            <button onClick={handleNext}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
